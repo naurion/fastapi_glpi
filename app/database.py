d81cb2debbefa6d1bcb17c1aea2ff45f221ed8f5
Sergey McArov <naurion@yandex.ru>
2023-05-10 14:51:51 +0300

    Change port for postgresql

--- app/database.py
+++ app/database.py
@@ -9,13 +9,14 @@ import os
 load_dotenv()
 
 DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASS = os.getenv('DB_PASS')
 
 
 def connect_db():
-    engine = create_engine(f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/{DB_NAME}", echo=True, pool_size=10,
+    engine = create_engine(f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}", echo=True, pool_size=10,
                            max_overflow=20)
     session = Session(engine)
     return session
